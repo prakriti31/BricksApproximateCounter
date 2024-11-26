c2eb046e76b86906ce55f66f312552e76cc58252
Prakriti  Sharma <prakritisharma@Prakritis-MacBook-Pro.local>
2024-11-26 13:41:05 -0600

    Initial commit with local changes

---
+++ src/main/java/com/example/bricksapproxcounter/TwoPhaseCohortMain3.java
@@ -0,0 +1,59 @@
+package main.java.com.example.bricksapproxcounter;
+
+import boundary.EventDrivenFLX.BO_InitClient;
+import boundary.EventDrivenFLX.BO_InitServer;
+import boundary.EventDrivenFLX.SynchronousSocketBoundaryObjectFeature;
+import bricks.MultiReqCausalCohortFeaturePackage;
+import main.BricksStrategy;
+import main.SyncCoordinator;
+
+public class TwoPhaseCohortMain3 {
+
+	public static void main(String[] args) {
+
+//		InetSocketAddress serverhostAddress = new InetSocketAddress("localhost", 11116);
+//		InetSocketAddress clienthostAddress = new InetSocketAddress("localhost", 11115);
+//		TcpChannelFeature coordinator = new TcpChannelFeature(serverhostAddress, clienthostAddress, null);
+////		TwoPhaseCohortFeature cohort = new TwoPhaseCohortFeature(new WrapperSender(coordinator), null);
+////		ForwardCohortFeaturePackage cohort = new ForwardCohortFeaturePackage(new WrapperSender(coordinator), null, new WrapperSender(coordinator));
+//		MultiRequestsCohortFeaturePackage cohort = new MultiRequestsCohortFeaturePackage(new WrapperSender(coordinator), null);
+//
+//		coordinator.handler = new AcceptHandler(cohort, new EventConverter());
+//
+//		coordinator.sendEvent(new InitServer());
+//		coordinator.sendEvent(new InitClient());
+		
+		String COORD_REMOTE = "localhost";
+
+	    int coordinatorPort = 11115;
+        int port = 11116;
+        
+        int chort1Port = 11119;
+        int port2 = 11120;
+		    
+	    SynchronousSocketBoundaryObjectFeature coordinator = new SynchronousSocketBoundaryObjectFeature(port, coordinatorPort, null, new BricksStrategy(), COORD_REMOTE);
+	    SynchronousSocketBoundaryObjectFeature cohort1 = new SynchronousSocketBoundaryObjectFeature(port2, chort1Port, null, new BricksStrategy(), COORD_REMOTE);
+
+//		TwoPhaseCohortFeature cohort = new TwoPhaseCohortFeature(new WrapperSender(coordinator), null);
+//		ForwardCohortFeaturePackage cohort = new ForwardCohortFeaturePackage(new WrapperSender(coordinator), null, new WrapperSender(coordinator));
+//		WrapperSender coordinatorWrapper = new WrapperSender(coordinator);
+//		VersioningForwardCohortFeaturePackage cohort = new VersioningForwardCohortFeaturePackage(coordinatorWrapper, null,coordinatorWrapper,null,100);
+//		MultiReqNoTPCForwardCohortFeaturePackage cohort = new MultiReqNoTPCForwardCohortFeaturePackage(coordinatorWrapper, null, coordinatorWrapper);
+
+        SyncCoordinator syncCoordinator = new SyncCoordinator(coordinator);
+
+//		MultiRequestsCohortFeaturePackage cohort = new MultiRequestsCohortFeaturePackage(syncCoordinator, null);
+//		MultiReqNoTPCForwardCohortFeaturePackage cohort = new MultiReqNoTPCForwardCohortFeaturePackage(syncCoordinator, null, null, null, null);
+		MultiReqCausalCohortFeaturePackage cohort = new MultiReqCausalCohortFeaturePackage(syncCoordinator, null, null, null, null, null, null);
+
+		coordinator.controlObject = cohort;
+
+        coordinator.sendEvent(new BO_InitServer());
+        coordinator.sendEvent(new BO_InitClient());
+        
+        cohort1.controlObject = cohort;
+        cohort1.sendEvent(new BO_InitServer());
+        cohort1.sendEvent(new BO_InitClient());
+	}
+
+}
